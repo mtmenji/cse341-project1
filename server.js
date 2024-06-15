df35cd02d91c73f3f214f01bc9f3ac98e7eea91b
mtmenji <mtmenji@gmail.com>
2024-06-15 17:00:51 -0600

    project1 complete

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(`server.js line 13: ${err}`)
+    } else {
+        app.listen(port, () => {console.log(`Database is listening. Node is running on port ${port}`)});
+    }
+})
